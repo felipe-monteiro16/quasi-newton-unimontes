708808da3ae54849c3f7a9ed5a74b1d08c897946
IuryOliveira567 <Iury.Oliveira567@gmail.com>
2026-06-16 11:10:26 -0300

    feat: added convergence chart  component

---
+++ frontend/src/components/ConvergenceChart.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="plot-container" ref="chart"></div>
+</template>
+
+<script setup>
+import Plotly from 'plotly.js-dist-min'
+import { ref, watch, onMounted } from 'vue'
+
+const props = defineProps({
+  data: Object
+})
+
+const chart = ref(null)
+
+function render() {
+
+  if (!props.data) return
+  if (!Array.isArray(props.data.f_iteracoes)) return
+
+  const iteracoes = props.data.f_iteracoes.map(
+    (_, i) => i + 1
+  )
+
+  Plotly.newPlot(
+    chart.value,
+    [
+      {
+        x: iteracoes,
+        y: props.data.f_iteracoes,
+        type: 'scatter',
+        mode: 'lines+markers'
+      }
+    ],
+    {
+      title: 'Convergência',
+      xaxis: {
+        title: 'Iteração'
+      },
+      yaxis: {
+        title: 'f(x)'
+      }
+    },
+    {
+      responsive: true,
+      displayModeBar: false
+    }
+  )
+}
+
+onMounted(() => {
+  render();
+})
+
+watch(
+  () => props.data,
+  render,
+  {}
+)
+
+</script>
+
+<style scoped>
+  .plot-container {
+    display: block;
+    margin: auto;
+    width:100%;
+    min-height: 420px;
+  }
+</style>
---
+++ frontend/src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as ConvergenceChart } from './ConvergenceChart.vue';
